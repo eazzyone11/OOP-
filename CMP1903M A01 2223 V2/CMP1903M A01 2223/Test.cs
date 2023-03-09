90d353fdf4d62858adbbd987f58f8f33812776b1
eazzyone11 <127356300+eazzyone11@users.noreply.github.com>
2023-03-09 14:48:13 +0000

    Add files via upload

---
+++ CMP1903M A01 2223 V2/CMP1903M A01 2223/Test.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    internal class Test : Validation
+    {
+       public Test()
+        {
+            //variables 
+            int valid;
+            int choice;
+            int numOfCards;
+            bool shuffled;
+
+            Pack pack = new Pack();
+            //asks the user for which card shuffle they would like 
+            valid = testing("Which card shuffle would you like : \n1. Fisher-Yates Shuffle \n2.Riffle Shuffle \n3.No Shuffle",0, 3);
+
+            shuffled = Pack.shuffleCardPack(valid);
+
+            if (shuffled == true)
+            {
+                //asks the user if they would like one or multiple cards 
+                choice = testing("How many cards would you like \n 1. Deal one \n2. Deal multiple", 0, 2);
+
+                if(choice == 1)
+                {
+                    Card card = Pack.deal();
+                    card.show();
+                }
+                else if (choice == 2)
+                {
+                    //asks the user for the quantity of the cards 
+                    numOfCards = testing("Please enter the amount of cards", 0, 52);
+                    List<Card> Cardslist = Pack.dealCard(numOfCards);
+                    foreach (Card cards in Cardslist)
+                    {
+                        cards.show();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("The card deck could not be shuffled"); 
+            }
+        }
+
+       
+
+    }
+}
---
+++ CMP1903M A01 2223 V2/CMP1903M A01 2223/Validation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    internal abstract class Validation
+    {
+       protected int testing(string message, int min, int max)
+        {
+            int validInput;
+            while(true)
+            {
+                Console.WriteLine(message);
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out validInput))
+                {
+                    //checks if the input is within the range 
+                    if (validInput > min && validInput <= max)
+                    {
+                        return validInput;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The value entered is outside the set boundaries");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid input");
+                }
+            }
+        }
+    }
+}
